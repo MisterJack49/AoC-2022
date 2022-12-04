e44ac7c68e01c04d74d08e73f2d6723161a84e93
MisterJack49 <MisterJack49@users.noreply.github.com>
2022-12-05 00:43:11 +0100

    Day4

---
+++ src/main/kotlin/days/Day4.kt
@@ -0,0 +1,29 @@
+package days
+
+class Day4 : Day(4) {
+
+    val reg = Regex("(\\d+)-(\\d+),(\\d+)-(\\d+)")
+    override fun partOne(): Any {
+        return inputList.mapNotNull { reg.find(it) }
+            .map {
+                Section(it.destructured.component1().toInt(), it.destructured.component2().toInt()) to
+                        Section(it.destructured.component3().toInt(), it.destructured.component4().toInt())
+            }.count { it.first.inbound(it.second) or it.second.inbound(it.first) }
+    }
+
+    override fun partTwo(): Any {
+        return inputList.mapNotNull { reg.find(it) }
+            .map {
+                Section(it.destructured.component1().toInt(), it.destructured.component2().toInt()) to
+                        Section(it.destructured.component3().toInt(), it.destructured.component4().toInt())
+            }.count { it.first.overlap(it.second) or it.second.overlap(it.first) }
+    }
+}
+
+private typealias Section = Pair<Int, Int>
+
+private fun Section.inbound(other: Section) =
+    this.first in other.first..other.second && this.second in other.first..other.second
+
+private fun Section.overlap(other: Section) =
+    this.first in other.first..other.second || this.second in other.first..other.second
