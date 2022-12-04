49d22d0aef1da060db4f2e2d8180f3ae4d3dfecf
MisterJack49 <MisterJack49@users.noreply.github.com>
2022-12-05 00:42:13 +0100

    Day1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,18 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        return inputString.split("\r\n\r\n")
+            .map { inventories -> inventories.split("\r\n") }
+            .map { inventory -> inventory.map { it.toInt() }.reduce { acc, x -> acc + x } }
+            .max()
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        return inputString.split("\r\n\r\n")
+            .map { inventories -> inventories.split("\r\n") }
+            .map { inventory -> inventory.map { it.toInt() }.reduce { acc, x -> acc + x } }
+            .sorted()
+            .takeLast(3)
+            .sum()
     }
 }
